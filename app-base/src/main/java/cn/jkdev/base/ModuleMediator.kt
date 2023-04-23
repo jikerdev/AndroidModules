b8b6af45df29ee7fc4219415b9fb6e12c58ec29e
jiker.dev <jiker.dev@gmail.com>
2023-04-24 01:28:39 +0800

    完成模块间相互跳转

---
+++ app-base/src/main/java/cn/jkdev/base/ModuleMediator.kt
@@ -0,0 +1,6 @@
+package cn.jkdev.base
+
+object ModuleMediator {
+    const val ACTIVITY_SETTING_CLASS = "cn.jkdev.setting.SettingActivity"
+    const val ACTIVITY_VIDEO_CLASS = "cn.jkdev.video.VideoActivity"
+}
