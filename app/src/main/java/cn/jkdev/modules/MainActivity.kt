9528ffb1be93bc6c49dbc195c227048f15c8a69f
panhy <phy.xyz@foxmail.com>
2023-04-23 20:07:57 +0800

    完成-定义全局的gradle文件

--- app/src/main/java/cn/jkdev/modules/MainActivity.kt
+++ app/src/main/java/cn/jkdev/modules/MainActivity.kt
@@ -4,19 +4,19 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import cn.jkdev.setting.SettingActivity
-import cn.jkdev.video.VideoActivity
+//import cn.jkdev.setting.SettingActivity
+//import cn.jkdev.video.VideoActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        findViewById<View>(R.id.bt_video).setOnClickListener {
-            startActivity(Intent(this, VideoActivity::class.java))
-        }
-        findViewById<View>(R.id.bt_setting).setOnClickListener {
-            startActivity(Intent(this, SettingActivity::class.java))
-        }
+//        findViewById<View>(R.id.bt_video).setOnClickListener {
+//            startActivity(Intent(this, VideoActivity::class.java))
+//        }
+//        findViewById<View>(R.id.bt_setting).setOnClickListener {
+//            startActivity(Intent(this, SettingActivity::class.java))
+//        }
     }
 }
